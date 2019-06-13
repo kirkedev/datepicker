22aef50ec9264ba9e21a02a3a27f01500a50949c
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2019-06-12 23:51:50 -0500

    Add last function

--- src/itertools/index.ts
+++ src/itertools/index.ts
@@ -21,3 +21,14 @@ export const slice = <T> (iterable: Iterable<T>, start: number, end: number) =>
 
 export const first = <T> (iterable: Iterable<T>) =>
     iterable[Symbol.iterator]().next().value;
+
+export function last<T>(iterable: Iterable<T>) {
+    const iterator = iterable[Symbol.iterator]();
+    let result = iterator.next();
+
+    while (!result.done) {
+        result = iterator.next();
+    }
+
+    return result.value;
+}
