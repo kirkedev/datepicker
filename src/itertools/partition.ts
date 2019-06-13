d07c957a5ddf4d297465cd90eb30033c3dfe0c0b
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2019-06-13 00:25:55 -0500

    Rename some stuff

--- src/itertools/partition.ts
+++ src/itertools/partition.ts
@@ -1,14 +1,14 @@
 import { UnaryOperator } from './index';
 
-function* partitionElements<T, R>(iterator: Iterator<T>, operator: UnaryOperator<T, R>): Iterator<Array<T>> {
+function* partitionElements<T, R>(iterator: Iterator<T>, selector: UnaryOperator<T, R>): Iterator<Array<T>> {
     let last;
     let index = 0;
     let result = iterator.next();
     const group = [];
 
     while (!result.done) {
-        const value = result.value;
-        const current = operator(value);
+        const { value } = result;
+        const current = selector(value);
 
         if (last !== undefined && current !== last) {
             yield group.slice(0, index);
