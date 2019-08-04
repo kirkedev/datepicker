74f95fe5f7f3be757438d725b63a5937659202ea
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2019-08-03 21:01:47 -0500

    Export reduce

--- src/itertools/index.ts
+++ src/itertools/index.ts
@@ -3,7 +3,7 @@ export type Predicate<T> = UnaryOperator<T, boolean>;
 export type BinaryOperator<T, U, R> = (first: T, second: U) => R;
 export type Accumulator<T, R> = BinaryOperator<R, T, R>;
 
-export { accumulate, count, countIf, sum, sumBy, all, none, some, one } from "./accumulate";
+export { accumulate, count, countIf, sum, sumBy, all, none, reduce, some, one } from "./accumulate";
 export { chunk } from "./chunk";
 export { drop, dropWhile, dropUntil } from "./drop";
 export { filter, find } from "./filter";
