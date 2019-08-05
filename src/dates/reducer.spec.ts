67f02e86b1e93cf6fc664f7e008096aa56c8eaa0
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2019-08-05 00:06:24 -0500

    Add assertions for all state properties in reducer tests

--- src/dates/reducer.spec.ts
+++ src/dates/reducer.spec.ts
@@ -6,28 +6,35 @@ test("go to previous calendar month", () => {
     const state = reducer(new DatePickerViewModel(6, 2019), previousMonth());
     expect(state.month).toBe(5);
     expect(state.year).toBe(2019);
+    expect(state.selected).toBeUndefined();
 });
 
 test("go to next calendar month", () => {
-    const state = reducer(new DatePickerViewModel(6, 2019), nextMonth());
+    const selected = new Date(2019, 5, 6);
+    const state = reducer(new DatePickerViewModel(6, 2019, selected), nextMonth());
     expect(state.month).toBe(7);
     expect(state.year).toBe(2019);
+    expect(state.selected).toEqual(selected);
 });
 
 test("go to previous month from January", () => {
     const state = reducer(new DatePickerViewModel(1, 2019), previousMonth());
     expect(state.month).toBe(12);
-    expect(state.year).toBe(2018)
+    expect(state.year).toBe(2018);
+    expect(state.selected).toBeUndefined();
 });
 
 test("go to next month from December", () => {
     const state = reducer(new DatePickerViewModel(12, 2018), nextMonth());
     expect(state.month).toBe(1);
-    expect(state.year).toBe(2019)
+    expect(state.year).toBe(2019);
+    expect(state.selected).toBeUndefined();
 });
 
 test("select a date", () => {
     const date = new Date(2019, 5, 6);
     const state = reducer(new DatePickerViewModel(6, 2019), selectDate(date));
+    expect(state.month).toBe(6);
+    expect(state.year).toBe(2019);
     expect(state.selected).toEqual(date);
 });
